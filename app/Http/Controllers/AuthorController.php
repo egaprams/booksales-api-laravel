d34ca7d20881462f54c338f0e46c9c7bb7f6c395
Aditya Ega Pratama <pratamaega513@gmail.com>
2025-05-25 13:37:23 +0700

    tugas pertemuan 5

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -48,4 +48,80 @@ class AuthorController extends Controller
             'data' => $author
         ], 201);
     }
+
+    public function show(string $id)
+    {
+        $author = Author::find($id);
+
+        if (!$author) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Resource not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get detail resource',
+            'data' => $author
+        ], 200);
+    }
+
+    public function update(string $id, Request $request)
+    {
+        $author = Author::find($id);
+        if (!$author) {
+            return response()->json([
+                'succes' => false,
+                'mesage' => 'resource not found!'
+            ], 404);
+        }
+        // 1. validator
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'bio' => 'required|string'
+        ]);
+
+        // 2. check validator error
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        // 3. siapkan data yang ingin diupdate
+        $data = [
+            'name' => $request->name,
+            'bio' => $request->bio,
+        ];
+
+        // 4. update data baru ke database
+        $author->update($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Resource updated successfully!',
+            'data' => $author
+        ], 200);
+    }
+
+    public function destroy(string $id)
+    {
+        $author = Author::find($id);
+
+        if (!$author) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Resource not found'
+            ], 404);
+        }
+
+        $author->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Delete resource successfully'
+        ]);
+    }
 }
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\Validator;
 
 class GenreController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $genres = Genre::all();
 
         return response()->json([
@@ -47,5 +48,81 @@ class GenreController extends Controller
             'message' => 'Resource added successfully!',
             'data' => $genre
         ], 201);
+    }
+
+    public function show(string $id)
+    {
+        $genre = Genre::find($id);
+
+        if (!$genre) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Resource not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get detail resource',
+            'data' => $genre
+        ], 200);
+    }
+
+    public function update(string $id, Request $request)
+    {
+        $genre = Genre::find($id);
+        if (!$genre) {
+            return response()->json([
+                'succes' => false,
+                'mesage' => 'resource not found!'
+            ], 404);
+        }
+        // 1. validator
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'description' => 'required|string'
+        ]);
+
+        // 2. check validator error
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        // 3. siapkan data yang ingin diupdate
+        $data = [
+            'name' => $request->name,
+            'description' => $request->description,
+        ];
+
+        // 4. update data baru ke database
+        $genre->update($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Resource updated successfully!',
+            'data' => $genre
+        ], 200);
+    }
+
+    public function destroy(string $id)
+    {
+        $genre = Genre::find($id);
+
+        if (!$genre) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Resource not found'
+            ], 404);
+        }
+
+        $genre->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Delete resource successfully'
+        ]);
     }
 }
